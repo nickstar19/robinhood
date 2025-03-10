52bae323cbb3215938aa1398479dbef026a0c32c
Sampadak-04 <kothari.sampada33@gmail.com>
2025-03-10 19:37:51 +0530

    added bg image

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-
+import '../style.css';
 const Home = () => {
   localStorage.removeItem('user');
   return (
     <div>
       <Navbar/>
+      <div className='bg'></div>
     </div>
   )
 }
